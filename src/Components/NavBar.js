259f8b95033b6e1944770b206c236f367d650825
summerhip <56841221+summerhip@users.noreply.github.com>
2022-05-05 14:36:43 -0400

    name update again

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -12,7 +12,7 @@ export function NavBar() {
 
     return (
         <div>
-            <a href='' className='logo'>SUM</a>
+            <a href='' className='logo'>SUMMER</a>
             <ul className='navigation'>
                 <div className='burger'>
                     <button onClick={toggleBurger}><GiHamburgerMenu size='25px'/></button>
