53b6d704e55aba43731e7708e47412b3c7a8f590
summerhip <56841221+summerhip@users.noreply.github.com>
2022-06-02 17:31:13 -0400

    experience update

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -8,16 +8,25 @@ export function Experience() {
             <ScrollAnimation animateIn='bounce' initiallyVisible={true} >
               <h1>Experience</h1>
             </ScrollAnimation>
+            <h4>Intact Technology <span class="date">May 2022 - August 2022</span></h4>
+            <p>Intern</p>
+            <ul>
+              <li>Learned and developed knowledge about the ServiceNow platform</li>
+              <li>Gathered and documented requirements</li>
+              <li>Developed and configured application reports, dashboards, flows, and processes in ServiceNow</li>
+              <li>Assisted with Learning and Development platform UI design using GoMo and Bridge LMS</li>
+            </ul>
+            <br/><br /><hr /><br />
+            <h4>By Shear Design Salon and Spa <span class="date">April 2018 - May 2022</span></h4>
+            <p>Part-Time Front Desk Receptionist and Shampoo Technician</p>
+            <ul>
+              <li>Provided customer oversight, enabled prompt service, and ensured client satisfaction</li>
+            </ul>
+            <br/><br /><hr /><br />
             <h4>George Mason University Tutoring Center <span class="date">January 2018 - January 2020</span></h4>
             <p>Tutored peers in Programming, Linear Algebra, Calculus I, II, and III</p>
             <ul>
               <li>Motivated struggling students with my effective communication and used problem solving skills to teach complex concepts</li>
-            </ul>
-            <br/><br /><hr /><br />
-            <h4>By Shear Design Salon and Spa <span class="date">April 2018 - Present</span></h4>
-            <p>Part-Time Front Desk Receptionist and Shampoo Technician</p>
-            <ul>
-              <li>Provided customer oversight, enabled prompt service, and ensured client satisfaction</li>
             </ul>
         </div>
     );
